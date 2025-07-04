9e6d814df436731244f4bd5739c1c476365e2c51
orangepi <orangepi@orangepizero2w-06b57f4>
2025-07-04 12:13:16 +0300

    dynamic_log.txt modified - 07/04/2025 - 12:13:12 PM

--- printer_data/config/kpr-config/finished.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printer_path = "$(dirname "$(dirname "$(dirname "$(reallink -f "$0")")")")"
-
-# Echo "Finished" to the static_log file
-echo "Finished" > "$printer_path/config/kpr-config/static_log.txt"
--- printer_data/config/kpr-config/unlog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printer_path = "$(dirname "$(dirname "$(dirname "$(reallink -f "$0")")")")"
-
-# Clear log.txt and log file path to it
-file_path=$1
-echo "$file_path" > $printer_path/config/kpr-config/static_log.txt
-echo 0 > $printer_path/config/kpr-config/dynamic_log.txt
